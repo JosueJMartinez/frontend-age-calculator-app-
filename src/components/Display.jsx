e950337c7335425d1e95bc08faf0a40eb42dc6d0
JosueJMartinez <jmarti61@gmail.com>
2023-09-28 00:18:06 -0400

    added animation to characters

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,26 @@
 import styles from '../styles/Display.module.scss';
+import { useSpring, animated } from 'react-spring';
 
 export function Display({ date }) {
+	const fadeIn = useSpring({
+		// transform: date['year'] !== '' ? 'scale(1)' : 'scale(1)',
+		opacity: date['year'] !== '' ? 1 : 0,
+		config: { duration: 1000 },
+	});
+	const fadeOut = useSpring({
+		// transform: date['year'] !== '' ? 'scale(1)' : 'scale(1)',
+		opacity: date['year'] !== '' ? 0 : 1,
+		config: { duration: 1000 },
+	});
 	const loopThroughDate = () => {
 		const keys = Object.keys(date);
 
 		return keys.map(key => (
 			<li key={key}>
-				<span>{date[key] !== '' ? date[key] : '--'}</span> {date[key] === 1 ? key : `${key}s`}
+				<animated.span style={date[key] !== '' ? fadeIn : fadeOut}>
+					{date[key] !== '' ? date[key] : '--'}
+				</animated.span>
+				{date[key] === 1 ? key : `${key}s`}
 			</li>
 		));
 	};
